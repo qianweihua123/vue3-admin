f3719c7227dd84a0d78e8740f1c1f8f69d9328d0
qwh <15806293089@163.com>
2022-11-16 14:44:58 +0800

    feat: 初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+/*
+ * @Author: qwh 15806293089@163.com
+ * @Date: 2022-11-15 22:33:32
+ * @LastEditors: qwh 15806293089@163.com
+ * @LastEditTime: 2022-11-16 12:21:22
+ * @FilePath: /vue3-admin/.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  "env": { // 环境，针对哪些环境的语法
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [ // 集成了哪些规则，别人写好的规则直接拿来用
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [],
+  "parser": "vue-eslint-parser", //解析.vue文件
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser", // 解析.ts文件
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue",
+    "@typescript-eslint",
+    "eslint-plugin-prettier"
+  ],
+  "rules": {
+    // 我们自定义的规则写在这里
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false, // 使用单引号
+        semi: false, // 末尾添加分号
+        tabWidth: 2,
+        trailingComma: "none",
+        useTabs: false,
+        endOfLine: "auto",
+      },
+    ],
+  }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: qwh 15806293089@163.com
+ * @Date: 2022-11-15 21:18:32
+ * @LastEditors: qwh 15806293089@163.com
+ * @LastEditTime: 2022-11-16 12:16:35
+ * @FilePath: /vue3-admin/src/components/HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup lang="ts">
+import { ref } from "vue"
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
